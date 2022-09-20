d43ede439e36bf34565b4c2803fdaea56c7be0de
Caio Barreto <Caio.BezerraLondresBarreto@windriver.com>
2022-09-20 12:09:13 -0300

    feature(master): finished all cruds functions

--- src/controllers/TransitionController.ts
+++ src/controllers/TransitionController.ts
@@ -1,3 +1,4 @@
+import { prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import { CreateTransition } from "../repositories/TransitionRepository";
 
@@ -17,6 +18,82 @@ export class TransitionController {
         message: 'Transition created',
         data: transition,
       };
+
+      return next();
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async readAll(res: Response, next: NextFunction) {
+    try {
+      const transitionRepository = new CreateTransition();
+      const transitions = await transitionRepository.getAll();
+
+      res.locals = {
+        status: 200,
+        data: transitions
+      }
+
+      return next();
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async read(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { transitionId } = req.params;
+      const transitionRepository = new CreateTransition();
+      const transition = await transitionRepository.getById(transitionId);
+
+      res.locals = {
+        status: 200,
+        data: transition
+      }
+
+      return next();
+    } catch (error) {
+      return next(error)
+    }
+  }
+
+  async patch(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { transitionId } = req.params;
+
+      const { type, title, value, category, date } = req.body;
+
+      const transitionData = { type, title, value, category, date };
+  
+      const transitionRepository = new CreateTransition();
+
+      const transition = await transitionRepository.update(transitionId, transitionData);
+
+      res.locals = {
+        message: 'Transition patched',
+        status: 200,
+        data: transition
+      }
+
+      return next();
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async delete(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { transitionId } = req.params;
+
+      const transitionRepository = new CreateTransition();
+
+      await transitionRepository.delete(transitionId);
+
+      res.locals = {
+        message: 'Transition deleted',
+        status: 200
+      }
 
       return next();
     } catch (error) {
--- src/repositories/TransitionRepository.ts
+++ src/repositories/TransitionRepository.ts
@@ -2,6 +2,13 @@ import { prisma } from "../prisma";
 import { Transition } from "@prisma/client";
 import { CreateTransitionDTO } from "../DTOs/TransitionDTO";
 
+type TransitionType = {
+  type: string;
+	title: string;
+	value: number;
+	category: string;
+	date: Date;
+}
 export class CreateTransition {
   async create({
     type, title, value, category, date
@@ -18,5 +25,22 @@ export class CreateTransition {
     })
 
     return transition;
+  }
+
+  async getAll(): Promise<Transition[]> {
+    return await prisma.transition.findMany();
+  }
+
+  async getById(id: string): Promise<Transition | null> {
+    return await prisma.transition.findUnique({ where: { id } });
+  }
+
+  async delete(id: string) {
+    return await prisma.transition.delete({ where: { id } });
+  }
+
+  async update(id: string, transitionData: TransitionType): Promise<Transition | null> {
+    await prisma.transition.update({ where: { id }, data: transitionData })
+    return await prisma.transition.findUnique({ where: { id } });
   }
 }
