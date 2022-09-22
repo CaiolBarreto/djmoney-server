14a417872fde35faab31f37ae6a203b7d31d44e7
Caio Barreto <Caio.BezerraLondresBarreto@windriver.com>
2022-09-21 21:50:57 -0300

    feature(master): changed some things in server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,9 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
-
 app.use(errorHandler);
 app.use(requestHandler);
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
