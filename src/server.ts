c4f7c8abf5e415d0d9b908dfddeb89f1bfefb8b7
Caio Barreto <Caio.BezerraLondresBarreto@windriver.com>
2022-09-25 23:13:38 -0300

    feature(master): change the items order on server

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,17 @@
 import express from 'express';
 import { routes } from './routes';
-const cors = require('cors')
+import cors from 'cors';
 import { requestHandler, errorHandler } from './middlewares';
 
 const app = express();
 
 app.use(express.json());
-
-app.use(routes);
-app.use(errorHandler);
 app.use(cors());
-app.use(requestHandler);
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
+app.use(errorHandler);
+app.use(requestHandler);
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
